af3711dc482d1edf7b48f6c9a986eb6b9b4225f8
jcstotomas <jcstotomas@yahoo.com>
2020-05-06 10:44:27 -0700

    test

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 if ["$TRAVIS_BRANCH" = "master"]; then
 	TAG = "latest"
--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import create_app
 
 application = create_app()
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     application.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
