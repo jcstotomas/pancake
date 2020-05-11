5c117e870dec1b3418a11d41f0cbd4f69593e204
jcstotomas <jcstotomas@yahoo.com>
2020-05-10 18:29:47 -0700

    Updated requirements

--- app/database/models.py
+++ app/database/models.py
@@ -6,3 +6,11 @@ class User(db.Model):
                    primary_key=True)
     name = db.Column(db.String(50))
     ssn = db.Column(db.String(10))
+
+
+
+class Posts(db.model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, foreign_key=True)
+    text_content = db.Column(db.String(150))
+    
