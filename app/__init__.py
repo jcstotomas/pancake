01a0e90615aeec20fc7b2a04ff2750dbe10b68da
jcstotomas <jcstotomas@yahoo.com>
2020-05-11 20:57:45 -0700

    added responses and primary key

--- app/__init__.py
+++ app/__init__.py
@@ -17,9 +17,9 @@ def create_app():
     app.config.from_object("app.config.Config")
     db.init_app(app)
     migrate = Migrate(app,db)
-
     from app.route_handlers import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api/v0.1')
-
+    with app.app_context():
+        db.create_all()
     return app
 
--- app/route_handlers/User.py
+++ app/route_handlers/User.py
@@ -1,12 +1,21 @@
 from ..database.models import User
 from app.route_handlers import bp
 from app import db
+from .response_template import success_response, error_response
 
-@bp.route("user", methods=["GET", "POST"])
+@bp.route("users", methods=["GET", "POST"])
 def create_user():
-    new_user = User(name="ASdf")
+    new_user = User(name="ASdf", email="asdff@yahoo.com", username="jeremy")
     db.session.add(new_user)
     db.session.commit()
-    return "created"
+    return success_response(200, message="User created successfully")
+
+
+@bp.route("users/<username>", methods=["GET"])
+def get_user(username):
+    user = db.session.query(User).filter_by(username=username).first()
+    print(user)
+    return str(user)
+
 
 
